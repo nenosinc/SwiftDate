e55fc322ed0ff65d257a0fa6cd518c521b8f9146
Sam Spencer <samspencer@hey.com>
2024-04-11 13:06:06 -0400

    Add visionOS, bump Swift version, remove resources

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,15 +1,11 @@
-// swift-tools-version:5.9
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "SwiftDate",
     defaultLocalization: "it",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .watchOS(.v6),
-        .tvOS(.v13),
-        .visionOS(.v1)
+        .macOS(.v10_15), .iOS(.v13), .watchOS(.v6), .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -20,8 +16,10 @@ let package = Package(
         .target(
             name: "SwiftDate",
             dependencies: [],
-            resources: []
-        ),
+            resources: [
+                .copy("Formatters/RelativeFormatter/langs"),
+                .process("Resources")
+            ]),
         .testTarget(
             name: "SwiftDateTests",
             dependencies: ["SwiftDate"])
